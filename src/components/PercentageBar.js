e957acd52da6c6a026a8feb04b6ec2d23f98bcb5
VaibhavTaragi <taragivaibhav@gmail.com>
2023-03-25 11:12:12 +0530

    Percentage in number added

--- src/components/PercentageBar.js
+++ src/components/PercentageBar.js
@@ -3,11 +3,12 @@ import React from "react";
 const PercentageBar = ({ percent }) => {
   return (
     <div className="flex justify-center m-4">
+      <div className="text-xs absolute">{percent}% Complete</div>
       <div className="bg-gray-200 w-1/2 h-4 rounded-full overflow-hidden">
         <div
           className="bg-green-500 h-full rounded-full transition-all duration-300"
-          style={{ width: `${percent}%` }}
-        ></div>
+          style={{ width: `${percent}%` }}>
+          </div>
       </div>
     </div>
   );
